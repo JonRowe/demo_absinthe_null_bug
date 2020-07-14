8c4b6ab8860bbfe2ffe39c7cf4ef4b0d7c9fe870
Jon Rowe <hello@jonrowe.co.uk>
2020-07-14 12:48:12 +0100

    Add absinthe

--- mix.exs
+++ mix.exs
@@ -33,6 +33,9 @@ defmodule DemoAbsintheNullBug.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:absinthe, "~> 1.5.0"},
+      {:absinthe_phoenix, "~> 2.0.0"},
+      {:absinthe_plug, "~> 1.5.0"},
       {:phoenix, "~> 1.5.3"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
